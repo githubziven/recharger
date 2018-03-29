79cfefa3c08d385c4cdc00f07b11327ed7144cb4
钟伟 <1240500584@qq.com>
2018-03-29 09:42:09 +0800

    commit project

---
+++ packages/lead/src/lead.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="r-lead">
+      <span v-if="$slots.default" ><slot></slot></span>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "RLead"
+    }
+</script>
+
+<style scoped>
+
+</style>
