3664801772f115bbfbb94027d9629e45e13c3d26
钟伟 <1240500584@qq.com>
2018-06-17 22:47:13 +0800

    no message

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: '',
-    assetsPublicPath: '/recharger',
+    assetsPublicPath: './',
 
     /**
      * Source Maps
